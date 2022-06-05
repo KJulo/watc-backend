a7cd364682f3c22c457442586e08830c73155019
Ignacio Silva <kjulo.c@gmail.com>
2022-06-04 23:49:29 -0400

    fix get '/'

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 const app = express();
 
-app.use(cors);
+//app.use(cors);
 require('dotenv').config();
 
+app.get('/', (req, res) => {
+  res.send("Hello World!");
+})
 
 app.listen(process.env.PORT || 8089, () => {
   console.log("server ready");
