c5da5944be93f0fd60472c71578e77ef44f100d6
Ignacio Silva <kjulo.c@gmail.com>
2022-06-15 00:35:08 -0400

    :sparkles: fixed seedPSQL

--- seedQuery.sql
+++ seedQuery.sql
@@ -1,46 +1,59 @@
-DROP TABLE IF EXISTS Estadios, Guardias, Inc_est, Incidencias, Usuarios, estadios, guardias, inc_est, incidencias, usuarios;
-DROP TABLE IF EXISTS estadios CASCADE;
+DROP TABLE IF EXISTS Estadios, Guardias, Incidencias, Usuarios, estadios, guardias, incidencias, usuarios CASCADE;
+DROP TABLE IF EXISTS usuarios CASCADE;
+CREATE TABLE usuarios (
+    id_usuario serial NOT NULL,
+    nombre varchar,
+    email varchar,
+    contrasena varchar,
+    id_incidencia int,
+    CONSTRAINT id_usuario_pk PRIMARY KEY (id_usuario)
+);
+
+DROP TABLE IF EXISTS incidencias CASCADE;
+CREATE TABLE incidencias (
+    id_incidencia serial NOT NULL,
+    tipo varchar,
+    descripcion varchar,
+    foto varchar,
+    fechaEmitida date,
+    ubicacion varchar,
+    isResuelta boolean,
+    id_usuario int,
+    id_estadio int,
+    CONSTRAINT id_incidencia_pk PRIMARY KEY (id_incidencia)
+);
+
 CREATE TABLE estadios (
-    id_estadio integer NOT NULL,
+    id_estadio serial NOT NULL,
     nombre varchar,
     mapa varchar,
     ubicacion varchar,
+    id_incidencia int,
+    id_guardia int,
     CONSTRAINT id_estadio_pk PRIMARY KEY (id_estadio)
 );
 
 DROP TABLE IF EXISTS guardias CASCADE;
 CREATE TABLE guardias (
-    id_guardia integer NOT NULL,
+    id_guardia serial NOT NULL,
     nombre varchar,
     email varchar,
     contrasena varchar,
+    id_estadio int,
     CONSTRAINT id_guardia_pk PRIMARY KEY (id_guardia)
 );
 
-DROP TABLE IF EXISTS incidencias CASCADE;
-CREATE TABLE incidencias (
-    id_incidencia integer NOT NULL,
-    tipo varchar,
-    descripcion varchar,
-    foto varchar,
-    realizado date,
-    ubicacion varchar,
-    CONSTRAINT id_incidencia_pk PRIMARY KEY (id_incidencia)
-);
+ALTER TABLE usuarios
+ADD CONSTRAINT id_incidencia_fk FOREIGN KEY(id_incidencia) REFERENCES incidencias(id_incidencia);
 
-DROP TABLE IF EXISTS usuarios CASCADE;
-CREATE TABLE usuarios (
-    id_usuario integer NOT NULL,
-    nombre varchar,
-    email varchar,
-    contrasena varchar,
-    CONSTRAINT id_usuario_pk PRIMARY KEY (id_usuario)
-);
+ALTER TABLE incidencias
+ADD CONSTRAINT id_usuario_fk FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+ADD CONSTRAINT id_estadio_fk FOREIGN KEY (id_estadio) REFERENCES estadios(id_estadio);
 
-DROP TABLE IF EXISTS inc_est CASCADE;
-CREATE TABLE inc_est (
-    id_incidencia integer not null,
-    id_estadio integer not null,
-    FOREIGN KEY (id_incidencia) REFERENCES incidencias(id_incidencia),
-    FOREIGN KEY (id_estadio) REFERENCES estadios(id_estadio)
-);
+ALTER TABLE estadios
+ADD CONSTRAINT id_incidencia_fk FOREIGN KEY (id_incidencia) REFERENCES incidencias(id_incidencia),
+ADD CONSTRAINT id_guardia_fk FOREIGN KEY (id_guardia) REFERENCES guardias(id_guardia);
+
+
+ALTER TABLE guardias
+ADD CONSTRAINT id_estadio_fk FOREIGN KEY (id_estadio) REFERENCES estadios(id_estadio);
