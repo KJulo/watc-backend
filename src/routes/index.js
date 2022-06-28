a5846bd94aa7ab588c4bfe6bb2448f01c2ea8aca
Ignacio Silva <kjulo.c@gmail.com>
2022-06-27 23:50:13 -0400

    :sparkles: added endpoint checkIncidenciasList

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,8 @@ router.get("/user", async (req, res) => {
   const response = await db.query("select * from usuarios");
   res.json({
     data: response.rows[0],
-  })
-})
+  });
+});
 
 router.get("/checkUsuarios", async (req, res) => {
   const response = await db.query(
@@ -17,6 +17,20 @@ router.get("/checkUsuarios", async (req, res) => {
   res.json({
     login: response.rows[0],
   });
+});
+
+router.get("/getIncidenciasList", async (req, res) => {
+  const response = await db.query(
+    "select tipo,descripcion,fechaemitida,ubicacion,isresuelta from incidencias"
+  );
+  res.json(response.rows);
+});
+
+router.get("/getIncidenciasListHours", async (req, res) => {
+  const response = await db.query(
+    "select tipo,descripcion,fechaemitida,ubicacion,isresuelta from incidencias where fechaemitida >= (now() - INTERVAL '2 hours')"
+  );
+  res.json(response.rows);
 });
 
 router.post("/addUsuario", async (req, res) => {
@@ -54,7 +68,16 @@ router.get("/checkAdministrador", async (req, res) => {
 
 router.post("/addIncidencia", async (req, res) => {
   try {
-    const response = await db.query("INSERT INTO incidencias(tipo,descripcion,fechaEmitida,isResuelta,ubicacion,id_usuario,id_estadio) VALUES($1,$2,NOW(),false,$3,$4,$5)", [req.body.incidencia.tipo, req.body.incidencia.descripcion, req.body.incidencia.ubicacion, req.body.user.ido, req.body.incidencia.idEstadio]);
+    const response = await db.query(
+      "INSERT INTO incidencias(tipo,descripcion,fechaEmitida,isResuelta,ubicacion,id_usuario,id_estadio) VALUES($1,$2,NOW(),false,$3,$4,$5)",
+      [
+        req.body.incidencia.tipo,
+        req.body.incidencia.descripcion,
+        req.body.incidencia.ubicacion,
+        req.body.user.ido,
+        req.body.incidencia.idEstadio,
+      ]
+    );
     console.log(response);
   } catch (error) {
     console.log(error);
@@ -63,11 +86,13 @@ router.post("/addIncidencia", async (req, res) => {
 
 router.get("/getIncidencias", async (req, res) => {
   try {
-    const response = await db.query("SELECT i.ubicacion, count (i.ubicacion) FROM incidencias i where i.ubicacion in (select nombre from ubicacion) GROUP BY i.ubicacion");
-    res.json(response.rows)
+    const response = await db.query(
+      "SELECT i.ubicacion, count (i.ubicacion) FROM incidencias i where i.ubicacion in (select nombre from ubicacion) GROUP BY i.ubicacion"
+    );
+    res.json(response.rows);
   } catch (error) {
     console.log(error);
   }
-})
+});
 
 module.exports = router;
